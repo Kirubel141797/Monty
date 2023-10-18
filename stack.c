bdd944631ed38481cfe931c1609097babe6e5255
kirubel141797 <kirubelteshome22@gmail.com>
2023-10-18 02:18:36 -0700

    kirubel teshome

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - To print the top
+ * @head: A stack head
+ * @counter: A line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
